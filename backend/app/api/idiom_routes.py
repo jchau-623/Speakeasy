6f75a3dfc078affdce17f4c63b3abb68b65a031a
justin chau <jchau623@gmail.com>
2024-07-23 18:24:13 -0400

    routes and models

---
+++ backend/app/api/idiom_routes.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, HTTPException, status
+from beanie import PydanticObjectId
+from models.idiom import Idiom, IdiomResponse
+
+idiom_router = APIRouter(
+    tags=["Idiom"]
+)
+
+@idiom_router.post("/", response_model=IdiomResponse)
+async def create_idiom(idiom: Idiom) -> Idiom:
+    idiom_exist = await Idiom.find_one(Idiom.idiom == idiom.idiom)
+    if idiom_exist:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Idiom already exists"
+        )
+
+    await idiom.insert()
+    return idiom
+
+
+@idiom_router.get("/{idiom_id}", response_model=IdiomResponse)
+async def get_idiom(idiom_id: PydanticObjectId) -> Idiom:
+    idiom = await Idiom.get(idiom_id)
+    if not idiom:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Idiom not found"
+        )
+    return idiom
+
+
+@idiom_router.put("/{idiom_id}", response_model=IdiomResponse)
+async def update_idiom(idiom_id: PydanticObjectId, idiom_data: Idiom) -> Idiom:
+    idiom = await Idiom.get(idiom_id)
+    if not idiom:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Idiom not found"
+        )
+
+    await idiom.update({"$set": idiom_data.dict()})
+    return idiom
+
+
+@idiom_router.delete("/{idiom_id}")
+async def delete_idiom(idiom_id: PydanticObjectId) -> dict:
+    idiom = await Idiom.get(idiom_id)
+    if not idiom:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Idiom not found"
+        )
+
+    await idiom.delete()
+    return {
+        "message": "Idiom deleted successfully"
+    }
