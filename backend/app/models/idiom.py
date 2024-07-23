db145ee7a942400fcf44857497c463e4c352a333
justin chau <jchau623@gmail.com>
2024-07-23 18:24:13 -0400

    routes and models

---
+++ backend/app/models/idiom.py
@@ -0,0 +1,23 @@
+from beanie import Document
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Idiom(Document):
+    idiom: str
+    language: str
+
+    class Settings:
+        name = "idioms"
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "idiom": "Break the ice",
+                "language": "en"
+            }
+        }
+
+
+class IdiomResponse(BaseModel):
+    idiom: str
+    language: str
