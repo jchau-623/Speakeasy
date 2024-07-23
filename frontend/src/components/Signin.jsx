86439ddd35b67940697c1cd3968645221272676a
Leslie Chou <chouyuchien@gmail.com>
2024-07-23 13:33:11 -0700

    modified welcome and signin style

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import robot from "../assets/chat-bot.gif";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+ 
 
 export default function SignIn() {
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isButtonActive, setIsButtonActive] = useState(false);
 
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(userName  + " " + password);
+    navigate("/home");
+  }
+
+
   useEffect(() => {
-    // Check if both userName and password are not empty to enable the button
     if (userName && password) {
       setIsButtonActive(true);
     } else {
@@ -19,21 +28,24 @@ export default function SignIn() {
   return (
     <>
       <div className="bg-[#F5FBF9] flex justify-center items-center w-full h-screen relative">
-        <img
-          src={robot}
-          alt="chat bot"
-          className="w-[8%] absolute top-[25%] left-[37%]"
-        />
-        <div className="animate__animated animate__fadeInUp w-[50%] flex flex-col gap-4 justify-center items-center relative">
-          <h1 className="text-5xl text-secondary font-bold mb-6">Sign In</h1>
-          <form className="flex flex-col gap-6">
+        <div className="animate__animated animate__fadeInUp w-[45%] flex flex-col gap-4 justify-center items-center">
+          <div className=" flex flex-col sm:flex sm:flex-row justify-center gap-8 relative">
+            <img
+              src={robot}
+              alt="chat bot"
+              className="w-[100px] sm:absolute -top-8 -left-[130px]"
+            />
+
+            <h1 className="text-5xl text-secondary font-bold mb-6">Sign In</h1>
+          </div>
+          <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
             <div>
               <input
                 type="email"
                 placeholder="User email"
                 value={userName}
                 onChange={(e) => setUsername(e.target.value)}
-                className="text-center text-xl p-2 rounded-full bg-secondary placeholder-white outline-none text-white font-semibold"
+                className="text-center text-sm sm:text-xl p-2 rounded-full bg-secondary placeholder-white outline-none text-white font-semibold"
               />
             </div>
             <div>
@@ -42,13 +54,13 @@ export default function SignIn() {
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="text-center text-xl p-2 rounded-full bg-secondary placeholder-white outline-none text-white font-semibold"
+                className="text-center text-sm sm:text-xl p-2 rounded-full bg-secondary placeholder-white outline-none text-white font-semibold"
               />
             </div>
 
             <button
               disabled={!userName || !password}
-              className={`bg-secondary w-fit mx-auto py-1 px-2 rounded-full disabled:bg-slate-300 disabled:text-white ${
+              className={`bg-secondary w-fit mx-auto py-1 px-2 rounded-full text-white text-sm sm:text-xl font-bold disabled:bg-slate-300 disabled:text-white ${
                 isButtonActive ? "animate__animated animate__rotateIn" : ""
               }`}
             >
@@ -56,12 +68,12 @@ export default function SignIn() {
             </button>
           </form>
 
-          <hr className="w-full h-2 bg-secondary rounded mb-6" />
+          <hr className="w-[200%] sm:w-full h-2 bg-secondary m-6" />
 
           <div>
             <Link
               to="/signup"
-              className="text-center py-2 px-6 rounded-full bg-secondary text-xl font-semibold"
+              className="text-center py-2 px-6 rounded-full bg-secondary text-sm sm:text-xl font-semibold"
             >
               Create an account
             </Link>
