593c6884185f3a411c7f9c7d12f9355bfc44ddb8
Leslie Chou <chouyuchien@gmail.com>
2024-07-29 10:00:58 -0700

    modified backend code the old one

--- backend/app/auth/jwt_handler.py
+++ backend/app/auth/jwt_handler.py
@@ -3,11 +3,6 @@ from datetime import datetime, timezone
 from fastapi import HTTPException, status
 from jose import jwt, JWTError
 from databases.connection import Settings
-#update line 6 - line10
-import logging #leslie  
-
-logging.basicConfig(level=logging.DEBUG) #leslie
-logger = logging.getLogger(__name__) #leslie
 
 settings = Settings()
 
@@ -20,20 +15,15 @@ def create_access_token(user: str) -> str:
     print("///////////SECRET_KEY///////////", settings.SECRET_KEY)
 
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
-    #update
-    logger.debug(f"Created token: {token}") #leslie
 
     return token
 
 
 def verify_access_token(token: str) -> dict:
     try:
-        logger.debug(f"Verifying token: {token}") #leslie
         data = jwt.decode(token, settings.SECRET_KEY, algorithms="HS256")
-        logger.debug(f"Decoded data: {data}") #leslie
 
         expire = data.get("expires")
-        logger.debug(f"Token expiration time: {expire}") #leslie
 
         if expire is None:
             raise HTTPException(
@@ -43,8 +33,6 @@ def verify_access_token(token: str) -> dict:
 
         now_utc = datetime.now(timezone.utc)
         expire_utc = datetime.fromtimestamp(expire, timezone.utc)
-        logger.debug(f"Current time (UTC): {now_utc}") #leslie
-        logger.debug(f"Token expiration time (UTC): {expire_utc}") #leslie
 
         if now_utc > expire_utc:
             raise HTTPException(
@@ -54,9 +42,7 @@ def verify_access_token(token: str) -> dict:
 
         return data
 
-    # except JWTError:
-    except JWTError as e: #leslie
-        logger.error(f"JWTError: {e}") #leslie
+    except JWTError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid token"
