29ba890299cbd51446fd8767bfef4bd92210dc56
justin chau <jchau623@gmail.com>
2024-07-31 13:21:50 -0400

    history route

--- backend/app/models/idiom.py
+++ backend/app/models/idiom.py
@@ -1,9 +1,10 @@
-from beanie import Document
+from beanie import Document, PydanticObjectId
 from pydantic import BaseModel
 
 class Idiom(Document):
     idiom: str
     language: str
+    user_id: PydanticObjectId
 
     class Settings:
         name = "idioms"
@@ -12,10 +13,12 @@ class Idiom(Document):
         schema_extra = {
             "example": {
                 "idiom": "Break the ice",
-                "language": "en"
+                "language": "en",
+                "user_id": "60f8f5f0a5c0d35a78b38b1d"
             }
         }
 
 class IdiomResponse(BaseModel):
     idiom: str
     language: str
+    user_id: PydanticObjectId
