8193a617c9d4c8509271a07d4e4aa43f35be91f6
justin chau <jchau623@gmail.com>
2024-07-31 20:04:48 -0400

    connection.py revert

--- backend/app/databases/connection.py
+++ backend/app/databases/connection.py
@@ -1,9 +1,10 @@
-from beanie import init_beanie, PydanticObjectId, Document
+from beanie import init_beanie, PydanticObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
-from typing import Optional, Type, List
+from typing import Optional, Any, List
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
-from models.idiom import Idiom
+from models.user import User
+
 
 class Settings(BaseSettings):
     SECRET_KEY: Optional[str] = None
@@ -12,33 +13,43 @@ class Settings(BaseSettings):
     async def initialize_database(self):
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(
-            database=client.speakeasy,
-            document_models=[Idiom]
+            database=client.get_default_database(),
+            document_models=[User]
         )
 
     class Config:
-        env_file = ".env.prod"
+        env_file = "../../.env"
+
 
 class Database:
-    def __init__(self, model: Type[Document]):
+    def __init__(self, model):
         self.model = model
 
-    async def save(self, document: BaseModel) -> None:
+    async def save(self, document) -> None:
         await document.create()
+        return
 
-    async def get(self, id: PydanticObjectId) -> Optional[Document]:
-        return await self.model.get(id)
+    async def get(self, id: PydanticObjectId) -> Any:
+        doc = await self.model.get(id)
+        if doc:
+            return doc
+        return False
 
-    async def get_all(self) -> List[Document]:
-        return await self.model.find_all().to_list()
+    async def get_all(self) -> List[Any]:
+        docs = await self.model.find_all().to_list()
+        return docs
 
-    async def update(self, id: PydanticObjectId, body: BaseModel) -> Optional[Document]:
-        des_body = {k: v for k, v in body.dict().items() if v is not None}
-        update_query = {"$set": des_body}
-        
-        doc = await self.get(id)
+    async def update(self, id: PydanticObjectId, body: BaseModel) -> Any:
+        doc_id = id
+        des_body = body.dict()
+        des_body = {k:v for k,v in des_body.items() if v is not None}
+        update_query = {"$set": {
+            field: value for field, value in des_body.items()
+        }}
+
+        doc = await self.get(doc_id)
         if not doc:
-            return None
+            return False
         await doc.update(update_query)
         return doc
 
@@ -48,6 +59,3 @@ class Database:
             return False
         await doc.delete()
         return True
-
-def get_database() -> Database:
-    return Database(Idiom)
