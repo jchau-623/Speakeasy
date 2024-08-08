952072ab75f4a0343a08767e96499f6c5046f1fd
justin chau <jchau623@gmail.com>
2024-08-08 19:12:22 -0400

    fixed import connection.py file

--- backend/app/databases/connection.py
+++ backend/app/databases/connection.py
@@ -1,6 +1,6 @@
-from beanie import init_beanie, PydanticObjectId
+from beanie import init_beanie, PydanticObjectId, Document
 from motor.motor_asyncio import AsyncIOMotorClient
-from typing import Optional, Any, List, Document
+from typing import Optional, Any, List
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from models.user import User
